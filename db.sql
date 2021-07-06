34ff04661e797aff6c8184baad737039ec177262
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-07-06 13:00:05 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,4 +1,19 @@
 CREATE DATABASE task_manager;
 CREATE ROLE db_user WITH LOGIN 'db_user' PASSWORD 'KQoEgwBi';
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255), 
+  email VARCHAR(255),
+  password VARCHAR(255) 
+);
+CREATE TABLE tasks(
+  id SERIAL PRIMARY KEY,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+  times TIMESTAMP WITH TIME ZONE ARRAY
+  tasks TEXT ARRAY
+);
+
+DROP TABLE tasks;
+DROP TABLE users;
 DROP ROLE db_user;
 DROP DATABASE task_manager;
