deed654c0adbd771680166f9701654dceb907e01
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-07-07 08:47:39 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,22 +1,22 @@
 CREATE DATABASE task_manager;
 CREATE ROLE admin WITH LOGIN PASSWORD 'KQoEgwBi';
 CREATE TABLE users(
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY DEFAULT random()*(2147483647-1+1))+1,
   name VARCHAR(255), 
   email VARCHAR(255),
   password VARCHAR(255) 
 );
 CREATE TABLE tasks(
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY DEFAULT random()*(2147483647-1+1))+1,
   user_id SERIAL,
   FOREIGN KEY (user_id) REFERENCES users (id),
   created TIMESTAMP WITH TIME ZONE,
   date_to_do TIMESTAMP WITH TIME ZONE,
   task TEXT,
-  is_priority BOOLEAN,
-  is_complete BOOLEAN,
-  is_cancel BOOLEAN,
-  is_delete BOOLEAN
+  is_priority BOOLEAN DEFAULT false,
+  is_complete BOOLEAN DEFAULT false,
+  is_cancel BOOLEAN DEFAULT false,
+  is_delete BOOLEAN DEFAULT false
 );
 INSERT INTO users VALUES (0, 'admin', 'admin@hey.com', 'hwegweKWHJEG');
 COPY users TO '/var/lib/postgresql/12/logs/task_manager/data.csv' WITH CSV DELIMITER ',';
