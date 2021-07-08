e5ae0fc5f5636041a4e43520a26570ae4c955a10
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-07-08 08:02:10 +0700

    db edit

--- db.sql
+++ db.sql
@@ -1,3 +1,14 @@
+CREATE FUNCTION get_new_id (OUT i INT, OUT res SERIAL) RETURNS SERIAL
+  i := 0;
+  WHILE i < 20 LOOP
+    res := random()*(2147483647-1+1))+1;
+    i := i + 1;
+    IF res  THEN
+      i := 100;
+    END IF;
+  END LOOP;
+$$ LANGUAGE SQL;
+
 CREATE DATABASE task_manager;
 CREATE ROLE admin WITH LOGIN PASSWORD 'KQoEgwBi';
 CREATE TABLE users(
@@ -20,7 +31,7 @@ CREATE TABLE tasks(
 );
 INSERT INTO users VALUES (1, 'admin', 'admin@hey.com', 'hwegweKWHJEG');
 COPY users TO '/var/lib/postgresql/12/logs/task_manager/data.csv' WITH CSV DELIMITER ',';
-INSERT INTO tasks VALUES (1, '1', 'admin@hey.com', 'hwegweKWHJEG');
+INSERT INTO tasks (user_id, created, date_to_do, task) VALUES (1, '2004-10-19 10:23:54+02', 'admin@hey.com', 'hwegweKWHJEG');
 COPY users TO '/var/lib/postgresql/12/logs/task_manager/data.csv' WITH CSV DELIMITER ',';
 SELECT * FROM users;
 SELECT * FROM tasks;
