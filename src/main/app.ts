a1528990158f8f1cbb4d2e6ce12a71f417c14ece
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-07-16 15:48:31 +0700

    creating express app

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,3 +1,19 @@
-export default function sum(a: number, b: number): number {
-    return a + b;
-}
+import express from 'express'
+import path from 'path'
+import {IConf} from './interfaces/config'
+import config from './config/default.json'
+
+config as IConf;
+
+const PORT: number = config.port || 4220;
+const app = express();
+const __dirname: string = path.dirname(__filename);
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.sendFile(path.resolve(__dirname, 'client', 'static', 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}...`)
+});
