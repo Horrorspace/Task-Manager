7b3676163514595f56d8e790d8fa65f742d3518b
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-03 12:24:35 +0700

    Update Footer.tsx

--- src/main/client/pages/react/components/Footer.tsx
+++ src/main/client/pages/react/components/Footer.tsx
@@ -3,34 +3,52 @@ import {Container, Row, Col, Button, Dropdown} from 'react-bootstrap'
 import {NavLink} from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {IconDefinition} from '@fortawesome/fontawesome-common-types'
-import { faCheckSquare } from '@fortawesome/free-regular-svg-icons'
+import { faSliders } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faCalendarCheck } from '@fortawesome/free-regular-svg-icons'
 
+interface ILink {
+    ico: IconDefinition;
+    link: string;
+    title: string;
+}
 
 export const Footer: React.FC = () => {
+    const linkDataDefault: ILink[] = [
+        {
+            ico: faCheckSquare,
+            link: '/',
+            title: 'Tasks'
+        },
+        {
+            ico: faCalendarCheck,
+            link: '/calendar',
+            title: 'Calendar'
+        },
+        {
+            ico: faSliders,
+            link: '/settings',
+            title: 'Settings'
+        }
+    ];
+    const [linksState, setLinks] = useState(linkDataDefault);
+    
+    
+    const Links: React.ReactElement[] = linksState.map((link: ILink): React.ReactElement => {
+    return (
+            <Col className="nav-item" sm="4">
+                <NavLink to={link.link} className="nav-link">
+                    <FontAwesomeIcon className="nav-ico" icon={link.ico} />
+                    <h2 className="nav-title">{link.title}</h2>
+                </NavLink>
+            </Col>
+        )
+    })
+    
     return (
         <Container as="footer" className="footer mx-auto" fluid>
             <Container as="nav" className="navigation d-flex justify-content-center align-items-center" fluid="lg">
                 <Row as="ul" className="nav-list d-flex justify-content-center">
-                    <Col className="nav-item" sm="4">
-                        <NavLink to="/" className="nav-link">
-                            <FontAwesomeIcon className="nav-ico" icon={faCheckSquare} />
-                            <h2 className="nav-title">Tasks</h2>
-                        </NavLink>
-                    </Col>
-
-                    <Col className="nav-item" sm="4">
-                        <NavLink to="/" className="nav-link">
-                            <FontAwesomeIcon className="nav-ico" icon={faCheckSquare} />
-                            <h2 className="nav-title">Tasks</h2>
-                        </NavLink>
-                    </Col>
-
-                    <Col className="nav-item" sm="4">
-                        <NavLink to="/" className="nav-link">
-                            <FontAwesomeIcon className="nav-ico" icon={faCheckSquare} />
-                            <h2 className="nav-title">Tasks</h2>
-                        </NavLink>
-                    </Col>
+                    {Links}
                 </Row>
             </Container>
         </Container>
