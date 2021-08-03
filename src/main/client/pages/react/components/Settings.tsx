c14433cdd3ef84e8de9bff66c688c97b881840e3
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-03 13:01:14 +0700

    Update Settings.tsx

--- src/main/client/pages/react/components/Settings.tsx
+++ src/main/client/pages/react/components/Settings.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export const Settings: React.FC = () => {
-    return <div>Hello5</div>
+    return (
+        <Container as="main" className="main" fluid>
+            <Container as="section" className="settings d-flex justify-content-center align-items-center" fluid="xl">
+                <h2 class="settings-title">Account</h2>
+                <Row as="ul" className="menu-list d-flex justify-content-center">
+                    
+                </Row>
+            </Container>
+        </Container>
+    )
 }
