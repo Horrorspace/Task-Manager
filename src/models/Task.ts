8074fb58f993a36d7d3e8304f006244e2270c4bf
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-31 12:30:48 +0700

    Update Task.ts

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -31,46 +31,14 @@ export default class Task extends PG {
         const dbData: QueryResult = await this.pool.query(`SELECT * FROM tasks WHERE user_id = ${id};`);
         const resultRows: QueryResultRow = dbData.rows;
         resultRows as ITaskResultRaw[];
-        const result: ITaskResult[] = resultRows.map((val: ITaskResultRaw): ITaskResult => {
-            return {
-                id: val.id,
-                userId: val.user_id,
-                created: val.created,
-                dateToDo: val.date_to_do,
-                dateOfComplete: val.date_complete,
-                dateOfCancel: val.date_cancel,
-                dateOfDelete: val.date_delete,
-                title: val.title,
-                task: val.task,
-                isPriority: val.is_priority,
-                isComplete: val.is_complete,
-                isCancel: val.is_cancel,
-                isDelete: val.is_delete,
-            }
-        })
+        const result: ITaskResult[] = resultRows.map((val: ITaskResultRaw): ITaskResult => this.converteResult(val));
         return result;
     }
     public async getUserPriorityTasks(id: number): Promise<ITaskResult[]> {
         const dbData: QueryResult = await this.pool.query(`SELECT * FROM tasks WHERE user_id = ${id} AND is_priority = ${true};`);
         const resultRows: QueryResultRow = dbData.rows;
         resultRows as ITaskResultRaw[];
-        const result: ITaskResult[] = resultRows.map((val: ITaskResultRaw): ITaskResult => {
-            return {
-                id: val.id,
-                userId: val.user_id,
-                created: val.created,
-                dateToDo: val.date_to_do,
-                dateOfComplete: val.date_complete,
-                dateOfCancel: val.date_cancel,
-                dateOfDelete: val.date_delete,
-                title: val.title,
-                task: val.task,
-                isPriority: val.is_priority,
-                isComplete: val.is_complete,
-                isCancel: val.is_cancel,
-                isDelete: val.is_delete,
-            }
-        })
+        const result: ITaskResult[] = resultRows.map((val: ITaskResultRaw): ITaskResult => this.converteResult(val));
         return result;
     }
     public async insertTask({email, dateToDo, title, task}: ITask): Promise<QueryResult> {
