cac07fa9e64c7f47bf53793214ded82c3c78f8d5
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-05 22:19:05 +0700

    edit app

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,141 @@ import express from 'express'
 import path from 'path'
 import {IConf} from './interfaces/config'
 import config from './config/default.json'
+import cors, { CorsOptions } from 'cors'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import passport from 'passport'
+import {localStrategy} from './auth/local'
+import {router as authRouter} from '../src/routes/auth.routes'
+import {IUser, IUserResult, IUserName, IUserEmail, IUserPass, IUserInstance} from 'interfaces/user'
+import User from './models/User'
+import { cookie } from 'express-validator'
+
 
 config as IConf;
 
-const PORT: number = config.port || 4220;
+const PORT: number = config.port || 3007;
 const app = express();
 const __dirname: string = path.dirname(__filename);
 
-app.get('/', (req, res) => {
-    console.log(req);
+const corsOptions: CorsOptions = {
+    origin: '*',
+    allowedHeaders: ['Content-Type',
+     'X-Custom-Header',
+     'x-requested-with',
+     'Host',
+     'User-Agent',
+     'Accept',
+     'Accept-Encoding',
+     'Accept-Language',
+     'Access-Control-Request-Headers',
+     'Access-Control-Request-Method',
+     'Proxy-Authorization',
+     'Proxy-Connection',
+     'Referer',
+     'Sec-Fetch-Mode',
+     'User-Agent',
+     'Connection'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+    credentials: true,
+    preflightContinue: true
+}
+
+app.use((req, res, next) => {
+    console.log(req.method);
+    next()
 })
 
-app.get('/app.js', (req, res) => {
-    console.log(req);
-})
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
-app.listen(PORT, () => {
-    console.log(`Server has been started on port ${PORT}...`)
+app.use(session({
+    secret: "cats and dogs",
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(localStrategy);
+passport.serializeUser((exUser: Express.User, done) => {
+    try {
+        const user = exUser as IUserResult
+        done(null, user.id)
+    }
+    catch(e) {
+      done(e)
+    }
+    
 });
+passport.deserializeUser(async (id: number, done) => {
+    try {
+        const users: IUserInstance = new User(config.PostgreSQL);
+        const userArr: IUserResult[] = await users.getUserById(id);
+        if(userArr.length === 0) {
+            throw 'Incorrect user id'
+        }
+        else {
+            console.log(userArr[0]);
+            done(null, userArr[0])
+        }
+    }
+    catch(e) {
+        done(e)
+    }
+});
+
+app.get('/', (req, res) => {
+    try {
+      const test = {
+        message: 'OK'
+      };
+      res.json(test);
+    } catch (e) {
+      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+    }
+})
+
+app.get(
+    '/test',
+    (req, res, next) => {
+        console.log(req.session.id, req.session.cookie, req.sessionID);
+        next()
+    },
+    (req, res) => {
+    try {
+      const test = {
+        message: 'test'
+      };
+      res.json(test);
+    } catch (e) {
+      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+    }
+})
+
+app.options('*', (req, res) => {
+    res.status(204).send();
+})
+
+app.use('/api/auth', authRouter);
+
+
+// app.get('*', (req, res) => {
+//     console.log(req.url);
+//     res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+// })
+  
+
+async function start(): Promise<void> {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server has been started on port ${PORT}...`);
+        });
+    }
+    catch(e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start();
