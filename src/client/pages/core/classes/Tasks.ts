f3ba84961f31560d68e1c762374d67715a9b1ac4
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-12 19:26:27 +0700

    create Tasks class

---
+++ src/client/pages/core/classes/Tasks.ts
@@ -0,0 +1,82 @@
+import {ITaskInstance, ITasksInstance} from '@interfaces/ITask'
+
+
+export default class Tasks implements ITasksInstance {
+    protected tasks: ITaskInstance[]
+
+    constructor(tasks: ITaskInstance[] | void) {
+        if(tasks) {
+            this.tasks = tasks
+        }
+        else {
+            this.tasks = []
+        }
+    }
+
+    public checkId(id: number): boolean {
+        return this.tasks.filter(task => task.getTaskId() === id).length > 0
+    }
+    public getAllTasks(): ITaskInstance[] {
+        return this.tasks
+    }
+    public getTaskById(id: number): ITaskInstance {
+        if(this.checkId(id)) {
+            return this.tasks.filter(task => task.getTaskId() === id)[0]
+        }
+        else {
+            throw 'Task with this id is not exist'
+        }
+    }
+    public getPriorityTasks(): ITaskInstance[] {
+        return this.tasks.filter(task => task.getPriority())
+    }
+    public getNonPriorityTasks(): ITaskInstance[] {
+        return this.tasks.filter(task => !task.getPriority())
+    }
+    public getCompleteTasks(): ITaskInstance[] {
+        return this.tasks.filter(task => task.getComplete())
+    }
+    public getNonCompleteTasks(): ITaskInstance[] {
+        return this.tasks.filter(task => !task.getComplete())
+    }
+    public getCancelTasks(): ITaskInstance[] {
+        return this.tasks.filter(task => task.getCancel())
+    }
+    public getNonCancelTasks(): ITaskInstance[] {
+        return this.tasks.filter(task => !task.getCancel())
+    }
+    public addTask(task: ITaskInstance): void {
+        if(!this.checkId(task.getTaskId())) {
+            this.tasks = [...this.tasks, task]
+        }
+        else {
+            throw 'Task id is not unique'
+        }
+    }
+    public editTask(task: ITaskInstance): void {
+        if(this.checkId(task.getTaskId())) {
+            const i: number = this.tasks.indexOf(task);
+            this.tasks = [...this.tasks.slice(0, i), task, ...this.tasks.slice(i + 1)]
+        }
+        else {
+            throw 'Task with this id is not exist'
+        }
+    }
+    public removeTask(task: ITaskInstance): void {
+        if(this.checkId(task.getTaskId())) {
+            const i: number = this.tasks.indexOf(task);
+            this.tasks = [...this.tasks.slice(0, i), ...this.tasks.slice(i + 1)]
+        }
+        else {
+            throw 'Task with this id is not exist'
+        }
+    }
+    public setTasks(tasks: ITaskInstance[] | void): void {
+        if(tasks) {
+            this.tasks = tasks
+        }
+        else {
+            this.tasks = []
+        }
+    }
+}
