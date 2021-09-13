90b3cb7a99d9b4fbe3fe28f68f1731d025c02b6f
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-09-13 12:53:46 +0700

    add testAPI function for API testing

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -2,5 +2,21 @@ import React from 'react'
 import {render} from 'react-dom'
 import '@scss/index.scss'
 import {App} from './react/App'
+import UserAPI from '@core/classes/UserAPI'
+
+const testUser = {
+  email: "tester@test.com",
+  password: "111111111"
+}
+
+async function testAPI() {
+  console.log(await UserAPI.getCurrentUser());
+  console.log(await UserAPI.toLogin(testUser));
+  console.log(await UserAPI.getCurrentUser());
+  console.log(await UserAPI.toLogout());
+  console.log(await UserAPI.getCurrentUser());
+}
+
+testAPI();
 
 render(<App />, document.getElementById('root'))
