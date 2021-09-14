aa7b6e57131573f299e6743652e23d540ea83f62
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-14 07:51:16 +0700

    edit api tests

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -3,20 +3,35 @@ import {render} from 'react-dom'
 import '@scss/index.scss'
 import {App} from './react/App'
 import UserAPI from '@core/classes/UserAPI'
+import TaskAPI from '@core/classes/TaskAPI'
+import { ILogin } from '@interfaces/IUser'
+import { INewTask } from '@interfaces/ITask'
 
-const testUser = {
+const testUser: ILogin = {
   email: "tester@test.com",
   password: "111111111"
 }
 
-async function testAPI() {
-  console.log(await UserAPI.getCurrentUser());
-  console.log(await UserAPI.toLogin(testUser));
-  console.log(await UserAPI.getCurrentUser());
-  console.log(await UserAPI.toLogout());
-  console.log(await UserAPI.getCurrentUser());
+const testTask: INewTask<string> = {
+  dateToDo: '2026-12-11 20:27:48+07',
+  email: testUser.email,
+  title: 'test',
+  task: 'testing'
 }
 
-testAPI();
+async function testAPI() {
+  //await UserAPI.toLogout();
+  await UserAPI.toLogin(testUser);
+  return await TaskAPI.downloadAllTasks();
+
+  //await UserAPI.toLogout();
+  // console.log(await UserAPI.getCurrentUser());
+  // console.log(await UserAPI.toLogout());
+  // console.log(await UserAPI.getCurrentUser());
+}
+
+testAPI().then(
+  d => console.log('ok', d)
+)
 
 render(<App />, document.getElementById('root'))
