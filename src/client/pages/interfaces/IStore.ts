bbdda117b4adfaa4457bcf062a04bcb2d5784c21
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-14 19:29:02 +0700

    save changes in IStore

--- src/client/pages/interfaces/IStore.ts
+++ src/client/pages/interfaces/IStore.ts
@@ -3,10 +3,10 @@ import {ITasksInstance} from '@interfaces/ITask'
 export interface ITaskState {
     tasks: ITasksInstance;
     isDataUpdating: boolean;
-    error: string;
+    error: string | null;
 }
 
 export interface IAppState {
     isDataUpdating: boolean;
-    error: string;
+    error: string | null;
 }
