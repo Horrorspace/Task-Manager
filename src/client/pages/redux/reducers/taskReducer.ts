41613141a19babedfadd40719b2e818577e1cb52
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-09-14 12:52:16 +0700

    Update taskReducer.ts

--- src/client/pages/redux/reducers/taskReducer.ts
+++ src/client/pages/redux/reducers/taskReducer.ts
@@ -1,6 +1,6 @@
 import {Reducer} from 'redux'
 import {TaskActTypes} from '@redux/types/TaskActTypes'
-import {ITaskState} from '@interfaces/IStore'
+import {ITaskState, ITaskAction} from '@interfaces/IStore'
 import Task from '@core/classes/Task'
 import Tasks from '@core/classes/Tasks'
 
@@ -8,29 +8,27 @@ import Tasks from '@core/classes/Tasks'
 const defaultState: ITaskState = {
     tasks: new Tasks(),
     isDataUpdating: false,
-    error: null
 };
 
-export const postReducer: Reducer = (state: ITaskState = defaultState, action: IreduxAction): IreduxState => {
+export const taskReducer: Reducer = (state: ITaskState = defaultState, action: ITaskAction): ITaskState => {
     switch (action.type) {
-        case PostActTypes.updatePosts:
-            console.log(action.posts);
-            if(action.posts) {
+        case TaskActTypes.setTasks:
+            if(action.tasks) {
                 return {
                     ...state,
-                    posts: action.posts,
+                    tasks: action.tasks,
                     isDataUpdating: false
                 }
             }
             else {
                 return state
             }
-        case PostActTypes.setDefault:
+        case TaskActTypes.setDefault:
             return defaultState
-        case PostActTypes.setUpdating:
+        case TaskActTypes.setUpdatingStatus:
                 return {
                     ...state,
-                    isDataUpdating: true
+                    isDataUpdating: action.isDataUpdating
                 }
         default:
             return state
