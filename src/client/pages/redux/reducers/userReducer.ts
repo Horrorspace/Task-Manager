08eab1c8c38efcb6ac3e1fef404c63822ccecd54
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-09-14 12:52:44 +0700

    Create userReducer.ts

---
+++ src/client/pages/redux/reducers/userReducer.ts
@@ -0,0 +1,36 @@
+import {Reducer} from 'redux'
+import {TaskActTypes} from '@redux/types/TaskActTypes'
+import {ITaskState, ITaskAction} from '@interfaces/IStore'
+import Task from '@core/classes/Task'
+import Tasks from '@core/classes/Tasks'
+
+
+const defaultState: ITaskState = {
+    tasks: new Tasks(),
+    isDataUpdating: false,
+};
+
+export const taskReducer: Reducer = (state: ITaskState = defaultState, action: ITaskAction): ITaskState => {
+    switch (action.type) {
+        case TaskActTypes.setTasks:
+            if(action.tasks) {
+                return {
+                    ...state,
+                    tasks: action.tasks,
+                    isDataUpdating: false
+                }
+            }
+            else {
+                return state
+            }
+        case TaskActTypes.setDefault:
+            return defaultState
+        case TaskActTypes.setUpdatingStatus:
+                return {
+                    ...state,
+                    isDataUpdating: action.isDataUpdating
+                }
+        default:
+            return state
+    }
+}
