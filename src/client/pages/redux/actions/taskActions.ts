0af08bc9d07865ed8efd68642931c750918dca50
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-09-15 12:38:51 +0700

    add action creators

--- src/client/pages/redux/actions/taskActions.ts
+++ src/client/pages/redux/actions/taskActions.ts
@@ -1,1 +1,52 @@
+import {TaskActTypes} from '@redux/types/TaskActTypes'
+import {ITaskAction, ITaskState, IThunkAction} from '@interfaces/IStore'
+import {INewTask, ITaskInstance, ITasksInstance} from '@interfaces/ITask'
+import TaskAPI from '@core/classes/TaskAPI'
+import {Dispatch} from 'redux'
 
+
+export const setTasks = (tasks: ITasksInstance): ITaskAction => {
+    return {
+        type: TaskActTypes.setTasks,
+        tasks
+    }
+};
+
+export const setDefault  = (): ITaskAction => {
+    return {
+        type: TaskActTypes.setDefault
+    }
+};
+
+export const setUpdatingStatus = (isDataUpdating: boolean): ITaskAction => {
+    return {
+        type: TaskActTypes.setUpdatingStatus,
+        isDataUpdating
+    }
+};
+
+export const setError = (error: string): ITaskAction => {
+    return {
+        type: TaskActTypes.setError,
+        error
+    }
+};
+
+export const setMessage = (message: string): ITaskAction => {
+    return {
+        type: TaskActTypes.setMessage,
+        message
+    }
+};
+
+export const clearError = (): ITaskAction => {
+    return {
+        type: TaskActTypes.clearError
+    }
+};
+
+export const clearMessage = (): ITaskAction => {
+    return {
+        type: TaskActTypes.clearMessage
+    }
+};
