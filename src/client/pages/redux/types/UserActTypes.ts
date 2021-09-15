032b832196d2e8e282d8c5c284684c7f9f1b67f8
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-09-15 12:43:35 +0700

    Update UserActTypes.ts

--- src/client/pages/redux/types/UserActTypes.ts
+++ src/client/pages/redux/types/UserActTypes.ts
@@ -1,9 +1,9 @@
 export enum UserActTypes {
     setUser = 'setUser',
-    setDefault = 'setDefault',
-    setUpdatingStatus = 'setUpdatingStatus',
-    setError = 'setError',
-    setMessage = 'setMessage',
-    clearError = 'clearError',
-    clearMessage = 'clearMessage',
+    setDefault = 'setDefault2',
+    setUpdatingStatus = 'setUpdatingStatus2',
+    setError = 'setError2',
+    setMessage = 'setMessage2',
+    clearError = 'clearError2',
+    clearMessage = 'clearMessage2',
 }
