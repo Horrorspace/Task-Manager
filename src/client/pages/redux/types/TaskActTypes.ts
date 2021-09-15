c798e367be6a9a1fbd4d6a6280902a44e025c275
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-09-15 12:43:38 +0700

    Update TaskActTypes.ts

--- src/client/pages/redux/types/TaskActTypes.ts
+++ src/client/pages/redux/types/TaskActTypes.ts
@@ -1,9 +1,9 @@
 export enum TaskActTypes {
     setTasks = 'setTasks',
-    setDefault = 'setDefault',
-    setUpdatingStatus = 'setUpdatingStatus',
-    setError = 'setError',
-    setMessage = 'setMessage',
-    clearError = 'clearError',
-    clearMessage = 'clearMessage',
+    setDefault = 'setDefault1',
+    setUpdatingStatus = 'setUpdatingStatus1',
+    setError = 'setError1',
+    setMessage = 'setMessage1',
+    clearError = 'clearError1',
+    clearMessage = 'clearMessage1',
 }
