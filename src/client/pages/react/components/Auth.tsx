de0e1570e90df2a5b377b4836923376efa066e76
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-09-16 13:03:19 +0700

    Update Auth.tsx

--- src/client/pages/react/components/Auth.tsx
+++ src/client/pages/react/components/Auth.tsx
@@ -1,3 +1,21 @@
 import React, {useState} from 'react'
 import {Container, Row, Col, Button} from 'react-bootstrap'
 import {NavLink} from 'react-router-dom'
+
+
+export const Auth: React.FC = () => {
+    return (
+        <Container as="main" className="main" fluid>
+            <Container as="section" className="auth d-flex flex-column justify-content-center align-items-center" fluid="xl">
+                <Container>  
+                  <h2>Email:</h2>
+                  <textarea></textarea>
+                </Container>  
+                <Container>  
+                  <h2>Password:</h2>
+                  <textarea></textarea>
+                </Container> 
+            </Container>
+        </Container>
+    )
+}
