03ba433e737c4b7245023dfbaf3594f1acb41354
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-16 19:47:51 +0700

    add app action creators

--- src/client/pages/redux/actions/appActions.ts
+++ src/client/pages/redux/actions/appActions.ts
@@ -1,3 +1,56 @@
 import {AppActTypes} from '@redux/types/AppActTypes'
-import {IAppAction, IAppState, IThunkAction, IDoubleThunk, IThunkDispatch} from '@interfaces/IStore'
-import {Dispatch} from 'redux'
+import {IAppAction} from '@interfaces/IStore'
+
+
+
+export const addError = (error: string): IAppAction => {
+    return {
+        type: AppActTypes.addError,
+        error
+    }
+};
+
+export const addMessage = (message: string): IAppAction => {
+    return {
+        type: AppActTypes.addMessage,
+        message
+    }
+};
+
+export const clearErrors = (): IAppAction => {
+    return {
+        type: AppActTypes.clearErrors,
+    }
+};
+
+export const clearMessages = (): IAppAction => {
+    return {
+        type: AppActTypes.clearMessages,
+    }
+};
+
+export const removeLastError = (): IAppAction => {
+    return {
+        type: AppActTypes.removeLastError,
+    }
+};
+
+export const removeLastMessage = (): IAppAction => {
+    return {
+        type: AppActTypes.removeLastMessage,
+    }
+};
+
+export const setNotifications = (notifications: boolean): IAppAction => {
+    return {
+        type: AppActTypes.setNotifications,
+        notifications
+    }
+};
+
+export const setUpdatingStatus = (isDataUpdating: boolean): IAppAction => {
+    return {
+        type: AppActTypes.setUpdatingStatus,
+        isDataUpdating
+    }
+};
