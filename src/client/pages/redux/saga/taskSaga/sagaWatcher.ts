a31764c792af7afa01a368c9ffea203113fab578
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-16 20:22:11 +0700

    create saga watcher for user

--- src/client/pages/redux/saga/taskSaga/sagaWatcher.ts
+++ src/client/pages/redux/saga/taskSaga/sagaWatcher.ts
@@ -1,6 +1,10 @@
 import {takeEvery} from 'redux-saga/effects'
+import {TaskActTypes} from '@redux/types/TaskActTypes'
+import errorSaga from '@redux/saga/taskSaga/errorSaga'
+import messageSaga from '@redux/saga/taskSaga/messageSaga'
 
 
 export default function* sagaWatcher() {
-
+    yield takeEvery(TaskActTypes.setError, errorSaga);
+    yield takeEvery(TaskActTypes.setMessage, messageSaga);
 }
