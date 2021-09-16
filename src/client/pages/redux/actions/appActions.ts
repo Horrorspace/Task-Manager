0b32ca9bfe5f5f995d83db1686c515d375505c6e
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-16 08:10:49 +0700

    create app actions file and add imports

---
+++ src/client/pages/redux/actions/appActions.ts
@@ -0,0 +1,3 @@
+import {AppActTypes} from '@redux/types/AppActTypes'
+import {IAppAction, IAppState, IThunkAction, IDoubleThunk, IThunkDispatch} from '@interfaces/IStore'
+import {Dispatch} from 'redux'
