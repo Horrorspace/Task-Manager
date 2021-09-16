8061029a3526a406216aa2360e73c3162e23f000
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-16 22:08:09 +0700

    create saga watcher for app

--- src/client/pages/redux/saga/appSaga/sagaWatcher.ts
+++ src/client/pages/redux/saga/appSaga/sagaWatcher.ts
@@ -1,6 +1,20 @@
 import {takeEvery} from 'redux-saga/effects'
+import {AppActTypes} from '@redux/types/AppActTypes'
+import {TaskActTypes} from '@redux/types/TaskActTypes'
+import {UserActTypes} from '@redux/types/UserActTypes'
+import appUpSaga from '@redux/saga/appSaga/appUpSaga'
+import errorSaga from '@redux/saga/appSaga/errorSaga'
+import messageSaga from '@redux/saga/appSaga/messageSaga'
+
 
 
 export default function* sagaWatcher() {
-
+    yield takeEvery([
+        TaskActTypes.setUpdatingStatus,
+        TaskActTypes.setDefault,
+        UserActTypes.setUpdatingStatus,
+        UserActTypes.setDefault,
+    ], appUpSaga);
+    yield takeEvery(AppActTypes.addMessage, messageSaga);
+    yield takeEvery(AppActTypes.addError, errorSaga);
 }
