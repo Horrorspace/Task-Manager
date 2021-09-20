54577f87066dcc4bb38fdb0351b6baa7d59114e9
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-20 07:51:38 +0700

    edit index imports

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import '@scss/index.scss'
-import {App} from './react/App'
+import {App} from '@react/App'
 import UserAPI from '@core/classes/UserAPI'
 import TaskAPI from '@core/classes/TaskAPI'
 import { ILogin } from '@interfaces/IUser'
