394266af01ccb7e57f942208d66b9b3dc1e23ea1
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-20 07:51:02 +0700

    edit App imports

--- src/client/pages/react/App.tsx
+++ src/client/pages/react/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import {Calendar} from './components/Calendar'
-import {Footer} from './components/Footer'
-import {Header} from './components/Header'
-import {Main} from './components/Main'
-import {Settings} from './components/Settings'
-import {TasksList} from './components/TasksList'
+import {Provider} from 'react-redux'
+import {Router} from '@react/Router'
+import {store} from '@redux/store'
+import {CookiesProvider} from 'react-cookie'
+
 
 export const App: React.FC = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <Switch>
-                <Route component={Main} path="/" exact />
-                <Route component={TasksList} path="/List" />
-                <Route component={Calendar} path="/Calendar" />
-                <Route component={Settings} path="/Settings" />
-            </Switch>
-            <Settings />
-            <Footer />
-        </BrowserRouter>
-    )
+        <CookiesProvider>
+          <Provider store={store}>
+            <Router />
+          </Provider>
+        </CookiesProvider>
+      )
 }
