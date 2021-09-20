7dab64b88d83030ad6fab26e8afedd299f07b333
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-09-20 07:46:34 +0700

    edit Settings

--- src/client/pages/react/components/Settings.tsx
+++ src/client/pages/react/components/Settings.tsx
@@ -1,104 +1,177 @@
 import React, {useState, ReactElement, MouseEvent} from 'react'
-import {Container, Row, Col, Button, Dropdown} from 'react-bootstrap'
+import {Container, Row, Col, Button, Dropdown, Modal} from 'react-bootstrap'
+import {useSelector, useDispatch} from 'react-redux'
+import {toLogout} from '@redux/actions/userActions'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {IconDefinition} from '@fortawesome/fontawesome-common-types'
-import { faMobileAlt, faKey, faGlobeEurope, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
+import { faMobileAlt, faKey, faGlobeEurope, faVolumeUp, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope, faBell } from '@fortawesome/free-regular-svg-icons'
 import { faTelegramPlane } from '@fortawesome/free-brands-svg-icons'
+
 
 interface ISetting {
     ico: IconDefinition;
     title: string;
-    value: string;
-    values: string[];
+    value: string | boolean;
     group: string;
+    handler: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
+
 export const Settings: React.FC = () => {
-    const groups: string[] = ['Account', 'Personal'];
-    const settingsDataDefault: ISetting[] = [
+    const dispatch = useDispatch();
+    const groups: string[] = ['Account', 'Personal', 'Authorization'];
+    const languageList: string[] = ['English', 'Russian'];
+    const booleanList: string[] = ['Yes', 'No']
+
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [telegram, setTelegram] = useState('');
+    const [language, setLanguage] = useState(languageList[0]);
+    const [notification, setNotification] = useState(true);
+    const [sound, setSound] = useState(true);
+    const [logoutShow, setLogoutShow] = useState(false);
+
+    const handleEmailClick = (event: MouseEvent<HTMLButtonElement>): void => {
+
+    }
+    const handlePhoneClick = (event: MouseEvent<HTMLButtonElement>): void => {
+
+    }
+    const handleNameClick = (event: MouseEvent<HTMLButtonElement>): void => {
+
+    }
+    const handleTelegramClick = (event: MouseEvent<HTMLButtonElement>): void => {
+
+    }
+    const handlePasswordClick = (event: MouseEvent<HTMLButtonElement>): void => {
+
+    }
+    const handleLogOutClick = (event: MouseEvent<HTMLButtonElement>): void => {
+        setLogoutShow(true)
+    }
+    const handleCloseLogout = (event: MouseEvent<HTMLButtonElement>): void => {
+        setLogoutShow(false)
+    }
+    const handleLogout = (event: MouseEvent<HTMLButtonElement>): void => {
+        dispatch(toLogout())
+    }
+    const handleLanguageClick = (event: MouseEvent<HTMLButtonElement>): void => {
+        setLanguage(prev => {
+            const i = languageList.indexOf(prev);
+            const l = languageList.length;
+            if(i+1 === l) {
+                return languageList[0]
+            }
+            else {
+                return languageList[i+1]
+            }
+        })
+    }
+    const handleNotificationClick = (event: MouseEvent<HTMLButtonElement>): void => {
+        setNotification(prev => !prev)
+    }
+    const handleSoundClick = (event: MouseEvent<HTMLButtonElement>): void => {
+        setSound(prev => !prev)
+    }
+    
+
+    const staticSettings: ISetting[] = [
         {
             ico: faEnvelope,
             title: 'Email',
-            value: 'admin@test.test',
-            values: ['admin@test.test'],
-            group: 'Account'
+            group: 'Account',
+            value: email,
+            handler: handleEmailClick
         },
         {
             ico: faMobileAlt,
             title: 'Phone',
-            value: '+79999999999',
-            values: ['+79999999999'],
-            group: 'Account'
+            group: 'Account',
+            value: phone,
+            handler: handlePhoneClick
+        },
+        {
+            ico: faUser,
+            title: 'Name',
+            group: 'Account',
+            value: name,
+            handler: handleNameClick
         },
         {
             ico: faTelegramPlane,
             title: 'Telegram',
-            value: 'Black Jesus',
-            values: ['Black Jesus'],
-            group: 'Account'
+            group: 'Account',
+            value: telegram,
+            handler: handleTelegramClick
         },
         {
             ico: faKey,
             title: 'Password',
-            value: '******',
-            values: ['******'],
-            group: 'Account'
+            group: 'Account',
+            value: password,
+            handler: handlePasswordClick
         },
         {
             ico: faGlobeEurope,
             title: 'Language',
-            value: 'English',
-            values: ['English', 'Russian'],
-            group: 'Personal'
+            group: 'Personal',
+            value: language,
+            handler: handleLanguageClick
         },
         {
             ico: faVolumeUp,
             title: 'Sound',
-            value: 'No',
-            values: ['No', 'Yes'],
-            group: 'Personal'
+            group: 'Personal',
+            value: notification ? booleanList[0] : booleanList[1],
+            handler: handleNotificationClick
         },
         {
             ico: faBell,
             title: 'Notifications',
-            value: 'No',
-            values: ['No','Yes'],
-            group: 'Personal'
+            group: 'Personal',
+            value: sound ? booleanList[0] : booleanList[1],
+            handler: handleSoundClick
         },
+        {
+            ico: faSignOutAlt,
+            title: 'Log out',
+            group: 'Authorization',
+            value: '',
+            handler: handleLogOutClick
+        }
     ];
-    const [settingsState, setSettings] = useState(settingsDataDefault);
 
-    const handleSettingClick = (event: MouseEvent<HTMLButtonElement>): void => {
-        setSettings (prevSet => {
-            const target = event.target as HTMLButtonElement;
-            const titleEl = target.children[1] as HTMLTitleElement;
-            const valueEl = target.children[2] as HTMLParagraphElement;
-            const setting: ISetting = prevSet.filter(element => element.title === titleEl.innerText)[0];
-            const values: string[] = setting.values;
-            const valLen: number = setting.values.length;
-            
-            if(valLen > 1) {
-                const ind: number = values.indexOf(valueEl.innerText);
-                const newVal: string = ind !== valLen + 1 ? values[ind + 1] : values[0];
-                const newSetting: ISetting = {...setting, value: newVal};
-                const setInd: number = prevSet.indexOf(setting);
-                const newState: ISetting[] = [...prevSet.slice(0, setInd), newSetting, ...prevSet.slice(setInd + 1)];
-                return newState;
-            }
-            else {
-                return prevSet;
-            }
-        })
-    };
+    const logoutModal: ReactElement =
+            <Modal show={logoutShow} onHide={handleCloseLogout}>
+                <Modal.Header>
+                    <Modal.Title>
+                        Log out
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Do you want log out?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleLogout}>
+                        Yes
+                    </Button>
+                    <Button variant="secondary" onClick={handleCloseLogout}>
+                        No
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
 
     const SettingsList: ReactElement[][] = groups.map((group: string): ReactElement[] => {
-        return settingsState
+        return staticSettings
                 .filter((setting: ISetting): boolean => setting.group === group)
                 .map((setting: ISetting): ReactElement => {
                     return (
                         <Row as="li" className="setting-item">
-                            <Col as="button" className="setting-btn d-flex align-items-center btn btn-light" sm="10" onClick={handleSettingClick}>
+                            <Col as="button" className="setting-btn d-flex align-items-center btn btn-light" sm="10" onClick={setting.handler}>
                                 <FontAwesomeIcon className="setting-ico" icon={setting.ico} />
                                 <h3 className="setting-title">{setting.title}</h3>
                                 <p className="setting-value">{setting.value}</p>
@@ -124,6 +197,7 @@ export const Settings: React.FC = () => {
         <Container as="main" className="main" fluid>
             <Container as="section" className="settings d-flex flex-column justify-content-center align-items-center" fluid="xl">
                 {Settings}
+                {logoutModal}
             </Container>
         </Container>
     )
