9d28ca6e11b00ab5832803b8220e5e8a1ff32bc0
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-10-04 07:51:30 +0700

    add new routes

--- src/client/pages/react/hooks/useRoutes.tsx
+++ src/client/pages/react/hooks/useRoutes.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Switch, Route, Redirect} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
+import {setOnlyImportant, setOnlyToday} from '@redux/actions/appActions'
 import {Calendar} from '@react/components/Calendar'
 import {Main} from '@react/components/Main'
 import {Settings} from '@react/components/Settings'
@@ -8,13 +10,49 @@ import {Auth} from '@react/components/Auth'
 
 
 export const useRoutes = (isLogined: boolean): React.ReactElement => {
+    const dispatch = useDispatch();
+
     if(isLogined) {
         return (
             <Switch>
                 <Route component={Main} path="/" exact />
-                <Route component={TasksList} path="/List" />
-                <Route component={Calendar} path="/Calendar" />
-                <Route component={Settings} path="/Settings" />
+                <Route  
+                    render={() => {
+                        dispatch(setOnlyImportant(false));
+                        dispatch(setOnlyToday(false))
+
+                        return(
+                            <TasksList />
+                        )
+                    }}  
+                    path="/List"
+                    exact 
+                />
+                <Route  
+                    render={() => {
+                        dispatch(setOnlyImportant(true));
+                        dispatch(setOnlyToday(false))
+
+                        return(
+                            <TasksList />
+                        )
+                    }}  
+                    path="/Important"
+                    exact 
+                />
+                <Route 
+                    render={() => {
+                        dispatch(setOnlyImportant(false));
+                        dispatch(setOnlyToday(true))
+
+                        return(
+                            <TasksList />
+                        )
+                    }}
+                    path="/Today" 
+                    exact 
+                />
+                <Route component={Settings} path="/Settings" exact />
                 <Redirect to="/" />
             </Switch>
         )
