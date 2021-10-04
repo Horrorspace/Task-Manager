823b01dea583c9e571b48f84498c167933e11763
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-10-04 07:52:26 +0700

    create AddTask component

---
+++ src/client/pages/react/components/AddTask.tsx
@@ -0,0 +1,191 @@
+import React, {ChangeEvent, MouseEvent, ReactElement} from 'react'
+import {Row, Col, Button, Form, Modal, FormControl} from 'react-bootstrap'
+import Calendar from 'react-calendar'
+import {getLocalDataString, getLocalTimeString} from '@core/functions/dateConverte'
+
+
+interface AddTaskProps {
+    onHide?: () => void;
+    onTitleChange?: (event?: ChangeEvent<HTMLElement>) => void;
+    onTaskChange?: (event?: ChangeEvent<HTMLElement>) => void;
+    onCalendarClick?: (event?: MouseEvent<HTMLElement>) => void;
+    onTimeClick?: (event?: MouseEvent<HTMLElement>) => void;
+    onDateChange?: (value?: Date) => void;
+    onHourClick?: (event?: MouseEvent<HTMLElement>) => void;
+    onMinuteClick?: (event?: MouseEvent<HTMLElement>) => void;
+    onAddClick?: (event?: MouseEvent<HTMLElement>) => void;
+    onCloseClick?: (event?: MouseEvent<HTMLElement>) => void;
+    show?: boolean;
+    title?: string;
+    task?: string;
+    dateToDo?: Date;
+    calendarClasses?: string[];
+    timeClasses?: string[];
+}
+
+const defaultProps: AddTaskProps = {
+    onHide: () => {},
+    onTitleChange: () => {},
+    onTaskChange: () => {},
+    onCalendarClick: () => {},
+    onTimeClick: () => {},
+    onDateChange: () => {},
+    onHourClick: () => {},
+    onMinuteClick: () => {},
+    onAddClick: () => {},
+    onCloseClick: () => {},
+    show: false,
+    title: '',
+    task: '',
+    dateToDo: new Date(Date.now()),
+    calendarClasses: ['hidden'],
+    timeClasses: ['hidden']
+}
+
+export const AddTask: React.FC<AddTaskProps> = (
+    {
+        onHide,
+        onTitleChange,
+        onTaskChange,
+        onCalendarClick,
+        onTimeClick,
+        onDateChange,
+        onHourClick,
+        onMinuteClick,
+        onAddClick,
+        onCloseClick,
+        show,
+        title,
+        task,
+        dateToDo,
+        calendarClasses,
+        timeClasses
+    }: AddTaskProps = defaultProps
+) => {
+    
+    const hoursList: number[] = [];
+    const minutesList: number[] = [];
+    for(let i = 0; i < 24; i++) {
+        hoursList.push(i)
+    }
+    for(let i = 0; i < 60; i++) {
+        minutesList.push(i)
+    }
+
+    const hoursPicker: ReactElement = 
+        <Col as="ul">
+            {hoursList.map(hour => <li onClick={onHourClick} key={hour}>{hour}</li>)}
+        </Col>
+    const minutesPicker: ReactElement = 
+        <Col as="ul">
+            {minutesList.map(minute => <li onClick={onMinuteClick} key={minute}>{minute}</li>)}
+        </Col>
+    
+    
+    return (
+        <Modal 
+            show={show}
+            backdrop={true}
+            onHide={onHide}
+            className=""
+            dialogClassName="add-task-main"
+            contentClassName="add-task-wrap"
+            scrollable={true}
+            size="xl"
+            as="section"
+        >
+            <Modal.Header 
+                className="add-task-header"
+            >
+                <h2 className="add-task-main-title">
+                    Create New Task
+                </h2>
+            </Modal.Header>
+            <Modal.Body 
+                className="add-task-body"
+            >
+                <Form as="div">
+                    <Form.Group as="div">
+                        <Form.Label 
+                            as="h3"
+                            className="add-task-title"
+                        >
+                            Title
+                        </Form.Label>
+                        <Form.Control 
+                            as="textarea"
+                            placeholder="Task name"
+                            onChange={onTitleChange}
+                            value={title}
+                            className="add-task-title-area"
+                        />
+                    </Form.Group>
+                    <Form.Group as="div">
+                        <Form.Label 
+                            as="h3"
+                            className="add-task-title"
+                        >
+                            Task description
+                        </Form.Label>
+                        <Form.Control 
+                            as="textarea"
+                            placeholder="Task Note"
+                            onChange={onTaskChange}
+                            value={task}
+                            className="add-task-text-area"
+                        />
+                    </Form.Group>
+                    <Form.Group as="div">
+                        <Form.Label 
+                            as="h3"
+                            className="add-task-title"
+                        >
+                            Date to do
+                        </Form.Label>
+                        <Form.Control 
+                            as="button"
+                            onClick={onCalendarClick}
+                            className="add-task-date-btn"
+                        >
+                            {getLocalDataString(dateToDo!)}
+                        </Form.Control>
+                        <Calendar 
+                            className={calendarClasses!.join(' ')}
+                            onChange={onDateChange}
+                            value={dateToDo}
+                        />
+                    </Form.Group>
+                    <Form.Group as="div">
+                        <Form.Label 
+                            as="h3"
+                            className="add-task-title"
+                        >
+                            Time to do
+                        </Form.Label>
+                        <Form.Control 
+                            as="button" 
+                            onClick={onTimeClick}
+                            className="add-task-date-btn"
+                        >
+                            {getLocalTimeString(dateToDo!)}
+                        </Form.Control>
+                        <Row className={timeClasses!.join(' ')}>
+                            {hoursPicker}
+                            {minutesPicker}
+                        </Row>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer
+                className="add-task-footer"
+            >
+                <Button variant="primary" onClick={onAddClick}>
+                    Add
+                </Button>
+                <Button variant="danger" onClick={onCloseClick}>
+                    Cancel
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
