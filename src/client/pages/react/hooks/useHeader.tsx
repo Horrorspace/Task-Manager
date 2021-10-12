954858410b4230cff88a8ba747f056960501e366
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-10-12 19:19:44 +0700

    add new routes to useHeader hook

--- src/client/pages/react/hooks/useHeader.tsx
+++ src/client/pages/react/hooks/useHeader.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Container, Row, Col, Button, Dropdown} from 'react-bootstrap'
 import {MainHeader} from '@react/components/MainHeader'
+import {SimpleHeader} from '@react/components/SimpleHeader'
+
 
 
 export const useHeader = (routeName: string): React.ReactElement => {
@@ -9,9 +11,47 @@ export const useHeader = (routeName: string): React.ReactElement => {
             return (
                 <Container as="header" className="header mx-auto" fluid />
             )
+        case '/List':
+            return (
+                <MainHeader 
+                    titleState="All Tasks"
+                />
+            )
+        case '/':
+            return (
+                <MainHeader 
+                    titleState="My Tasks"
+                />
+            )
+        case '/Important':
+            return (
+                <MainHeader 
+                    titleState="Important Tasks"
+                    hideOnlyImportant={true}
+                />
+            )
+        case '/Today':
+            return (
+                <MainHeader 
+                    titleState="Today Tasks"
+                    hideOnlyToday={true}
+                />
+            )
+        case '/Calendar':
+            return (
+                <MainHeader 
+                    titleState="Calendar"
+                />
+            )
+        case '/Settings':
+            return (
+                <SimpleHeader 
+                    titleState="Settings"
+                />
+            )
         default:
             return (
-                <MainHeader />
+                <Container as="header" className="header mx-auto" fluid />
             )
     }
 }
