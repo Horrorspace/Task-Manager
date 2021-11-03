e948c43fe377c45b54efac4bde231732bdb40218
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-03 20:23:43 +0700

    delete useLocation hook from component

--- src/client/pages/react/components/Footer.tsx
+++ src/client/pages/react/components/Footer.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import {BrowserRouter, useLocation} from 'react-router-dom'
 import {useFooter} from '@react/hooks/useFooter'
 
 
 export const Footer: React.FC = () => {
-    const routeName = useLocation().pathname;
-    const currentFooter = useFooter(routeName);
-
+    const currentFooter: React.ReactElement = useFooter();
     return (
         <>
             {currentFooter}
--- src/client/pages/react/components/Header.tsx
+++ src/client/pages/react/components/Header.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import {BrowserRouter, useLocation} from 'react-router-dom'
 import {useHeader} from '@react/hooks/useHeader'
 import {useSelector} from 'react-redux'
 import {IRootState} from '@interfaces/IStore'
-import {MainHeader} from '@react/components/MainHeader'
+
 
 export const Header: React.FC = () => {
-    const routeName = useLocation().pathname;
-    const currentHeader = useHeader(routeName);
-
+    const currentHeader: React.ReactElement = useHeader();
     const isUpdating = useSelector((state: IRootState): boolean => state.app.isDataUpdating);
     const app = document.getElementById('root');
     if(isUpdating) {
