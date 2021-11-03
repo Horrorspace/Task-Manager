b448063bf79453d459be2c9f10cb691a148ca072
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-03 20:21:55 +0700

    delete args from hook and add useLoation hook in useFooter

--- src/client/pages/react/hooks/useFooter.tsx
+++ src/client/pages/react/hooks/useFooter.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {useLocation} from 'react-router-dom'
 import {Container, Row, Col, Button, Dropdown} from 'react-bootstrap'
 import {MainFooter} from '@react/components/MainFooter'
 
 
-export const useFooter = (routeName: string): React.ReactElement => {
+export const useFooter = (): React.ReactElement => {
+    const routeName = useLocation().pathname;
     switch(routeName) {
         case '/Auth':
             return (
